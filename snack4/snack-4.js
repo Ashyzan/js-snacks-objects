eba5a7534e566c1e9d90de019f51c87da7929c17
Ashyzan <ashyzan@gmail.com>
2024-03-22 19:55:24 +0100

    ADD: snack 4 result

--- snack4/snack-4.js
+++ snack4/snack-4.js
@@ -1,1 +1,64 @@
-console.log('funziona')
+//console.log('funziona')
+
+// Crea un array di oggetti che rappresentano delle persone.
+// Ogni persona ha un nome, un cognome e un’età.
+
+const persone = [
+    {
+        nome: 'Gisella',
+        cognome: 'Mannucci',
+        eta: '18',
+    },
+
+    {
+        nome: 'Marco',
+        cognome: 'Mannucci',
+        eta: '28',
+    },
+
+    {
+        nome: 'Leone',
+        cognome: 'Mannucci',
+        eta: '5',
+    },
+
+    {
+        nome: 'Paolo',
+        cognome: 'Mannucci',
+        eta: '12',
+    },
+
+    {
+        nome: 'Carlo',
+        cognome: 'Paolini',
+        eta: '38',
+    },
+
+
+
+]
+
+//console.log(persone)
+
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il
+// nome e cognome e l’indicazione se può guidare, in base all’età.
+
+const arrayGuida = []
+
+for ( i = 0 ; i < persone.length; i++) {
+
+    const persona = persone[i]
+
+    //console.log(persona)
+
+    if ( persona.eta >= 18 ) {
+
+        console.log(persona.nome, persona.cognome)
+
+        console.log('può guidare')
+
+    }
+
+}
+
+
